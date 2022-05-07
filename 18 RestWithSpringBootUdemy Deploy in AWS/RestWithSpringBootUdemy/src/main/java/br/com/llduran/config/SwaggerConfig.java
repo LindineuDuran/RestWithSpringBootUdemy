39d49548c443034f75d5277056e2bd0802772d6e
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-05-07 18:56:52 -0300

    Implantação na Amazon AWS

---
+++ 18 RestWithSpringBootUdemy Deploy in AWS/RestWithSpringBootUdemy/src/main/java/br/com/llduran/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package br.com.llduran.config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.LinkDiscoverer;
+import org.springframework.hateoas.LinkDiscoverers;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig
+{
+	@Bean
+	public Docket api()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.llduran"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo()
+	{
+		return new ApiInfoBuilder()
+				.title("RestWithSpringBootUdemy")
+				.description("REST API's RESTFul do 0 à AWS com Spring Boot 2.x e Docker")
+				.version("v1")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.contact(new springfox.documentation.service.Contact("Lindineu Duran",
+						"https://github.com/LindineuDuran?tab=repositories", "lduran355@gmail.com"))
+				.build();
+	}
+}
