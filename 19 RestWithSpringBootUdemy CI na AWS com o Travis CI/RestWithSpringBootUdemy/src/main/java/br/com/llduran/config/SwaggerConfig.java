697fb5b988bedbcd2cab17b1133dff189f4b0597
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-05-14 16:40:13 -0300

    Alteração em SwaggerConfig.java

--- 19 RestWithSpringBootUdemy CI na AWS com o Travis CI/RestWithSpringBootUdemy/src/main/java/br/com/llduran/config/SwaggerConfig.java
+++ 19 RestWithSpringBootUdemy CI na AWS com o Travis CI/RestWithSpringBootUdemy/src/main/java/br/com/llduran/config/SwaggerConfig.java
@@ -38,7 +38,7 @@ public class SwaggerConfig
 	private ApiInfo apiInfo()
 	{
 		return new ApiInfoBuilder()
-				.title("RestWithSpringBootUdemy - Bubaloo")
+				.title("RestWithSpringBootUdemy")
 				.description("REST API's RESTFul do 0 à AWS com Spring Boot 2.x e Docker")
 				.version("v1")
 				.license("Apache License Version 2.0")
