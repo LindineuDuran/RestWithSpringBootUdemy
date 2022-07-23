de5ba21c36d84f477c1406be5b1f07bdb6ceb3bb
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-07-23 11:45:36 -0300

    Entendendo Path Params e o Tratamento de Exceptions no Spring Boot 3

---
+++ Spring Boot 3.x/06_ParametersAndExceptionHandler/rest-with-spring-boot-udemy/src/main/java/br/com/llduran/math/SimpleMath.java
@@ -0,0 +1,34 @@
+package br.com.llduran.math;
+
+public class SimpleMath
+{
+	public Double sum(Double numberOne, Double numberTwo)
+	{
+		return numberOne + numberTwo;
+	}
+
+	public Double subtraction(Double numberOne, Double numberTwo)
+	{
+		return numberOne - numberTwo;
+	}
+
+	public Double multiplication(Double numberOne, Double numberTwo)
+	{
+		return numberOne * numberTwo;
+	}
+
+	public Double division(Double numberOne, Double numberTwo)
+	{
+		return numberOne / numberTwo;
+	}
+
+	public Double mean(Double numberOne, Double numberTwo)
+	{
+		return (numberOne + numberTwo) / 2;
+	}
+
+	public Double squareRoot(Double number)
+	{
+		return Math.sqrt(number);
+	}
+}
