c5904b0e977bc5b84a43d8179a18bff66a3be675
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-07-24 20:25:44 -0300

    Custom JSON Serialization

--- Spring Boot 3.x/12_CustomSerialization/rest-with-spring-boot-udemy/src/main/java/br/com/llduran/data/vo/v2/PersonVOV2.java
+++ Spring Boot 3.x/12_CustomSerialization/rest-with-spring-boot-udemy/src/main/java/br/com/llduran/data/vo/v2/PersonVOV2.java
@@ -1,23 +1,19 @@
-package br.com.llduran.data.vo.v1;
+package br.com.llduran.data.vo.v2;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
-public class PersonVO implements Serializable
+public class PersonVOV2 implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
-
-	@JsonProperty("first_name")
 	private String firstName;
-
-	@JsonProperty("last_name")
 	private String lastName;
-
 	private String address;
 	private String gender;
+	private Date birthDay;
 }
